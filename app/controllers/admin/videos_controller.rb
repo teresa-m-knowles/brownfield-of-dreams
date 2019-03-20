2bd3257bd03d6fcb1a87f567a41695d77dd38e26
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-20 14:50:31 -0600

    changes video_id to id and refactors create action in videos controller

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -9,20 +9,34 @@ class Admin::VideosController < Admin::BaseController
   end
 
   def create
-    begin
-      tutorial  = Tutorial.find(params[:tutorial_id])
-      thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
-      video     = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
+    tutorial = Tutorial.find(params[:tutorial_id])
+    video = tutorial.videos.new(new_video_params)
 
-      video.save
-
+    if video.save
       flash[:success] = "Successfully created video."
-    rescue # Sorry about this. We should get more specific instead of swallowing all errors.
+      redirect_to edit_admin_tutorial_path(tutorial)
+    else
       flash[:error] = "Unable to create video."
+      render :new
     end
-
-    redirect_to edit_admin_tutorial_path(id: tutorial.id)
   end
+
+  # def create
+  #   binding.pry
+  #   begin
+  #     tutorial  = Tutorial.find(params[:tutorial_id])
+  #     thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
+  #     video     = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
+  #
+  #     video.save
+  #
+  #     flash[:success] = "Successfully created video."
+  #   rescue # Sorry about this. We should get more specific instead of swallowing all errors.
+  #     flash[:error] = "Unable to create video."
+  #   end
+  #
+  #   redirect_to edit_admin_tutorial_path(id: tutorial.id)
+  # end
 
   private
     def video_params
@@ -30,6 +44,6 @@ class Admin::VideosController < Admin::BaseController
     end
 
     def new_video_params
-      params.require(:video).permit(:title, :description, :video_id, :thumbnail)
+      params.require(:video).permit(:title, :description, :thumbnail)
     end
 end
